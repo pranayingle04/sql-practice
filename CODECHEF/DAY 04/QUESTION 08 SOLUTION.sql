0ec0c11532b2cf9b7d9022f9f0f10201b4d36869
pranayingle04 <168402473+pranayingle04@users.noreply.github.com>
2026-02-15 23:32:23 +0530

    Add files via upload

---
+++ CODECHEF/DAY 04/QUESTION 08 SOLUTION.sql
@@ -0,0 +1,1 @@
+SELECT book_id, title, author, published_year FROM Library WHERE RATING ISNULL;
---
+++ CODECHEF/DAY 04/QUESTION 09 SOLUTION.sql
@@ -0,0 +1,1 @@
+SELECT employee_name, company, salary FROM Employees WHERE category = 'Full-Time' ORDER BY salary DESC;
